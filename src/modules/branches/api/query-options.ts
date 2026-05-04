33fa7a6a2cdfdd75d1b26aa14bef6b9d7acde438
Paul2G <pggaleana@gmail.com>
2026-05-03 19:59:38 -0700

    refactor(branches): add lookup route query

--- src/modules/branches/api/query-options.ts
+++ src/modules/branches/api/query-options.ts
@@ -3,6 +3,7 @@ import type { BranchesSearchParams } from '@/modules/branches/types';
 import { queryOptions } from '@tanstack/react-query';
 
 import {
+  getAllBranches,
   getBranchById,
   getBranchesList,
 } from '@/modules/branches/api/query-fns';
@@ -12,6 +13,14 @@ export const branchesIndexQueryOptions = (params: BranchesSearchParams) =>
     queryKey: ['branches', params],
     queryFn: () => getBranchesList(params),
   });
+
+export const branchesLookupQueryOptions = () => {
+  return queryOptions({
+    queryKey: ['branches', 'lookup'],
+    queryFn: () => getAllBranches(),
+    staleTime: 1000 * 60, // 1 minute to avoid reload on multiple forms in a short time
+  });
+};
 
 export const branchQueryOptions = (itemId: number) =>
   queryOptions({
