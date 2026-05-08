2138a3896b151791f146b3f35130755f794d10a9
Paul2G <pggaleana@gmail.com>
2026-05-07 20:00:29 -0700

    feat: add under construction message to unfinished routes

--- src/routes/app/dashboard.tsx
+++ src/routes/app/dashboard.tsx
@@ -1,23 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import { UnderConstruction } from '@/layout/components/under-construction';
 import { createRouteHead } from '@/layout/lib/create-route-head';
-import { useAuth } from '@/modules/auth/hooks/use-auth';
 
 export const Route = createFileRoute('/app/dashboard')({
-  component: DashboardPage,
+  component: UnderConstruction,
   head: createRouteHead({
     type: 'index',
     titleI18nKey: 'layout:navigation.dashboard',
   }),
 });
 
-function DashboardPage() {
-  const { user } = useAuth();
-
-  return (
-    <section className="grid gap-2 p-2">
-      <p>Hi {user?.username}!</p>
-      <p>You are currently on the dashboard route.</p>
-    </section>
-  );
-}
+// function DashboardPage() {
+//   const { user } = useAuth();
+//
+//   return (
+//     <section className="grid gap-2 p-2">
+//       <p>Hi {user?.username}!</p>
+//       <p>You are currently on the dashboard route.</p>
+//     </section>
+//   );
+// }
---
+++ src/routes/app/users/route.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+import { UnderConstruction } from '@/layout/components/under-construction';
+import { createRouteHead } from '@/layout/lib/create-route-head';
+
+export const Route = createFileRoute('/app/users')({
+  head: createRouteHead({ type: 'index', titleI18nKey: 'users:name' }),
+  component: UnderConstruction,
+});
+
+// function RouteComponent() {
+//   return <div>Hello "/app/users"!</div>
+// }
